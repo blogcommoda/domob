227e103d8c1879f80f0695e6bd944d51efa61434
Ed Baker <edward.david.baker@gmail.com>
2012-09-16 00:08:49 -0700

    Setting up cache directory on external directory. Also pop out of attempting to cache a file if the song is already cached

--- src/com/sound/ampache/MediaCache.java
+++ src/com/sound/ampache/MediaCache.java
@@ -11,19 +11,28 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
+import java.io.File;
 
 public class MediaCache
 {
   private DownloadManager dm;
   private long enqueue;
   private Context mContext;
+  private long maxCacheSize; /// This is the maximum amount of data to cache
+  private String cacheDir; /// Folder to store all of the local files
 
   MediaCache (Context mCtxt)
   {
     mContext = mCtxt;
     dm = (DownloadManager)mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+    maxCacheSize = 100*1024*1024; // 100 MiB
+
+    // Setup the directory to store the cache on the external storage
+    File externalMusicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+    File cacheDir = new File(externalMusicDir.getAbsolutePath() + "ampacheCache");
   }
 
   /** \brief Add a song to the local music cache.
@@ -31,7 +40,11 @@ public class MediaCache
    */
   public void cache_file(long songUid) throws Exception
   {
-
+    // If the song is already cached, we are already done
+    if (check_if_cached(songUid))
+    {
+      return;
+    }
   }
 
   /** \brief Check to see if a song is already in the local music cache.
