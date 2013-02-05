2b974c667cc04510f3d868041aa9aca5b2f54c5f
Ed Baker <edward.david.baker@gmail.com>
2013-02-04 23:58:18 -0800

    Update bitmap generation script to use inkscape

--- artwork/update-logo.sh
+++ artwork/update-logo.sh
@@ -2,12 +2,21 @@
 
 originalSvg="./logo.svg"
 outputImage="domob_icon.png"
-lDpiWidth=36
-mDpiWidth=48
-hDpiWidth=72
-xhDpiWidth=96
+# Paths corresponding to LDPI, MDPI, HDPI, XHDPI, and the developer console
+outPaths=("../res/drawable-ldpi/$outputImage" \
+          "../res/drawable-mdpi/$outputImage" \
+          "../res/drawable-hdpi/$outputImage" \
+          "../res/drawable-xhdpi/$outputImage" \
+          "./dev-$outputImage")
+# DPIs corresponding to LDPI, MDPI, HDPI, XHDPI, and the developer console
+dpiWidths=(36 48 72 96 512)
 
-convert -background none $originalSvg -resize $lDpiWidth ../res/drawable-ldpi/$outputImage
-convert -background none $originalSvg -resize $mDpiWidth ../res/drawable-mdpi/$outputImage
-convert -background none $originalSvg -resize $hDpiWidth ../res/drawable-hdpi/$outputImage
-convert -background none $originalSvg -resize $xhDpiWidth ../res/drawable-xhdpi/$outputImage
+for ((i=0; i < ${#outPaths[@]}; i++))
+do
+  inkscape --export-png=${outPaths[$i]} \
+           --export-width=${dpiWidths[$i]} \
+           --export-background-opacity=0 \
+           --without-gui \
+           $originalSvg
+done
+
