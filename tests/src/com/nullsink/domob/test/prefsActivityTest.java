2010b6c0d4fe71aec9b06c0d7fe8b7c035c274af
Ed Baker <edward.david.baker@gmail.com>
2013-04-03 00:15:21 -0700

    Testing that the preference fields exist.

--- tests/src/com/nullsink/domob/test/prefsActivityTest.java
+++ tests/src/com/nullsink/domob/test/prefsActivityTest.java
@@ -2,12 +2,16 @@ package com.nullsink.domob.test;
 
 import com.nullsink.domob.prefsActivity;
 
+import android.preference.Preference;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class prefsActivityTest extends
     ActivityInstrumentationTestCase2<prefsActivity> {
 
   private prefsActivity mActivity;
+  private Preference mServerUrlPreference;
+  private Preference mServerUsernamePreference;
+  private Preference mServerPasswordPreference;
 
   public prefsActivityTest() {
     super("com.nullsink.domob", prefsActivity.class);
@@ -20,6 +24,17 @@ public class prefsActivityTest extends
     setActivityInitialTouchMode(false);
 
     mActivity = getActivity();
+
+    mServerUrlPreference = mActivity.findPreference("server_url_preference");
+    mServerUsernamePreference = mActivity.findPreference("server_username_preference");
+    mServerPasswordPreference = mActivity.findPreference("server_password_preference");
+  }
+
+  public void testPreConditions() {
+    // First check that all of the preference fields exist
+    assertTrue(mServerUrlPreference != null);
+    assertTrue(mServerUsernamePreference != null);
+    assertTrue(mServerPasswordPreference != null);
   }
 }
 
