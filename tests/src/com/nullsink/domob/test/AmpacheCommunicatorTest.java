a77faacb89e28a47da6131dc91727f51c91b4b6f
Ed Baker <edward.david.baker@gmail.com>
2013-04-03 23:12:08 -0700

    Combining communication test methods

--- tests/src/com/nullsink/domob/test/AmpacheCommunicatorTest.java
+++ tests/src/com/nullsink/domob/test/AmpacheCommunicatorTest.java
@@ -18,8 +18,8 @@ public class AmpacheCommunicatorTest extends
   private static final String PROTOCOL_PREFERENCE_KEY = "server_protocol_preference";
   private static final String SERVER_USERNAME = "test";
   private static final String SERVER_PASSWORD = "test";
-  private static final String SERVER_URL_HTTP = "http://localhost/ampache";
-  private static final String SERVER_URL_NO_HTTP = "localhost/ampache";
+  private static final String[] SERVER_URLS = {"localhost/ampache",
+                                               "http://localhost/ampache"};
   private static final String[] SERVER_PROTOCOLS = {"http"};
 
   @Override
@@ -33,81 +33,64 @@ public class AmpacheCommunicatorTest extends
   public void testFetchFromServer() {
     // Loop through all of the protocols
     for (int x = 0; x < SERVER_PROTOCOLS.length; x++) {
-      // First set all of the shared preference values
-      SharedPreferences.Editor editor = mSharedPreferences.edit();
-      editor.putString(USERNAME_PREFERENCE_KEY, SERVER_USERNAME);
-      editor.putString(PASSWORD_PREFERENCE_KEY, SERVER_PASSWORD);
-      editor.putString(URL_PREFERENCE_KEY, SERVER_URL_HTTP);
-      editor.putString(PROTOCOL_PREFERENCE_KEY, SERVER_PROTOCOLS[x]);
-      editor.commit();
+      // Loop through all of the URLs as well
+      for (int y=0; y < SERVER_URLS.length; y++) {
+        // First set all of the shared preference values
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(USERNAME_PREFERENCE_KEY, SERVER_USERNAME);
+        editor.putString(PASSWORD_PREFERENCE_KEY, SERVER_PASSWORD);
+        editor.putString(URL_PREFERENCE_KEY, SERVER_URLS[y]);
+        editor.putString(PROTOCOL_PREFERENCE_KEY, SERVER_PROTOCOLS[x]);
+        editor.commit();
 
-      // With the preferences setup, create the communicator
-      try {
-        mAmpacheCommunicator = new ampacheCommunicator(mSharedPreferences, mCtxt);
-      } catch (Exception e) {
-        fail("Could not create ampacheCommunicator.");
-      }
+        // With the preferences setup, create the communicator
+        try {
+          mAmpacheCommunicator = new ampacheCommunicator(mSharedPreferences, mCtxt);
+        } catch (Exception e) {
+          fail("Could not create ampacheCommunicator.");
+        }
 
-      // Check to see if constructing the URL throws an exception
-      try {
-        mAmpacheCommunicator.fetchFromServer("");
-      } catch (Exception e) {
-        fail(e.getMessage());
+        // Check to see if constructing the URL throws an exception
+        try {
+          mAmpacheCommunicator.fetchFromServer("");
+        } catch (Exception e) {
+          fail(e.getMessage());
+        }
       }
     }
   }
 
-  public void testHttpUrl() {
-    // First set all of the shared preference values
-    SharedPreferences.Editor editor = mSharedPreferences.edit();
-    editor.putString(USERNAME_PREFERENCE_KEY, SERVER_USERNAME);
-    editor.putString(PASSWORD_PREFERENCE_KEY, SERVER_PASSWORD);
-    editor.putString(URL_PREFERENCE_KEY, SERVER_URL_HTTP);
-    editor.commit();
+  public void testPerformAuthRequest() {
+    // Loop through all of the protocols
+    for (int x = 0; x < SERVER_PROTOCOLS.length; x++) {
+      // Loop through all of the URLs as well
+      for (int y=0; y < SERVER_URLS.length; y++) {
+        // First set all of the shared preference values
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString(USERNAME_PREFERENCE_KEY, SERVER_USERNAME);
+        editor.putString(PASSWORD_PREFERENCE_KEY, SERVER_PASSWORD);
+        editor.putString(URL_PREFERENCE_KEY, SERVER_URLS[y]);
+        editor.putString(PROTOCOL_PREFERENCE_KEY, SERVER_PROTOCOLS[x]);
+        editor.commit();
 
-    // With the preferences setup, create the communicator
-    try {
-      mAmpacheCommunicator = new ampacheCommunicator(mSharedPreferences, mCtxt);
-    } catch (Exception e) {
-      fail("Could not create ampacheCommunicator.");
+        // With the preferences setup, create the communicator
+        try {
+          mAmpacheCommunicator = new ampacheCommunicator(mSharedPreferences, mCtxt);
+        } catch (Exception e) {
+          fail("Could not create ampacheCommunicator.");
+        }
+
+        // Try to connect to the server
+        try {
+          mAmpacheCommunicator.perform_auth_request();
+        } catch (Exception e) {
+          fail("Could not perform_auth_request.");
+        }
+
+        // If the connection was successful, the authtoken will be set
+        assertNotNull(mAmpacheCommunicator.authToken);
+        assertNotSame(mAmpacheCommunicator.authToken, "");
+      }
     }
-
-    // Try to connect to the server
-    try {
-      mAmpacheCommunicator.perform_auth_request();
-    } catch (Exception e) {
-      fail("Could not perform_auth_request.");
-    }
-
-    // If the connection was successful, the authtoken will be set
-    assertNotNull(mAmpacheCommunicator.authToken);
-    assertNotSame(mAmpacheCommunicator.authToken, "");
-  }
-
-  public void testNoHttpUrl() {
-    // First set all of the shared preference values
-    SharedPreferences.Editor editor = mSharedPreferences.edit();
-    editor.putString(USERNAME_PREFERENCE_KEY, SERVER_USERNAME);
-    editor.putString(PASSWORD_PREFERENCE_KEY, SERVER_PASSWORD);
-    editor.putString(URL_PREFERENCE_KEY, SERVER_URL_NO_HTTP);
-    editor.commit();
-
-    // With the preferences setup, create the communicator
-    try {
-      mAmpacheCommunicator = new ampacheCommunicator(mSharedPreferences, mCtxt);
-    } catch (Exception e) {
-      fail("Could not create ampacheCommunicator.");
-    }
-
-    // Try to connect to the server
-    try {
-      mAmpacheCommunicator.perform_auth_request();
-    } catch (Exception e) {
-      fail("Could not perform_auth_request.");
-    }
-
-    // If the connection was successful, the authtoken will be set
-    assertNotNull(mAmpacheCommunicator.authToken);
-    assertNotSame(mAmpacheCommunicator.authToken, "");
   }
 }
